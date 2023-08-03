c49dc6de5a756d1ba25add6e2128ce41bd0a1533
siyan liu <siyanliuads@gmail.com>
2023-08-03 01:16:26 -0500

    second commit

---
+++ battery/battery.py
@@ -0,0 +1,4 @@
+class Battery:
+    def __init__(self, last_service_date):
+        self.last_service_date = last_service_date
+    
--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,7 +1,15 @@
-from datetime import datetime
+from datetime import date
+from battery import Battery
 
-class NubbinBattery:
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+
+class NubbinBattery(Battery):
     def needs_service(self):
-        current_date = 
+        current_date = date.today()
+        if ((current_date - self.last_service_date).days >= 365 * 4):
+            return True
+        else:
+            return False
+
+last_date = date(2019, 7, 26)
+my_battery = NubbinBattery(last_date)
+print(my_battery.needs_service())
